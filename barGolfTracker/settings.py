bae36fc11dcdb5cff2d13ae99925f86e07a5cd70
Tyler Fritz <fritzt72@gmail.com>
2023-08-28 13:14:08 -0500

    changed location of whitenoise middleware

--- barGolfTracker/settings.py
+++ barGolfTracker/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 MIDDLEWARE = [
@@ -54,6 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "barGolfTracker.urls"
